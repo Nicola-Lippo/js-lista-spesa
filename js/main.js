533be81d7da5b3a72b655ef6d7080de02ee6c5f8
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-07 13:52:16 +0200

    non funziona

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+console.log('sono collegato');
+//creo un array per contenere la mia lista spesa
+const listaSpesa = ['pasta', 'pane', 'pizze', 'frutta', 'tonno', 'uova'];
+console.log(listaSpesa);
+//creo un ciclo while per leggere la lista automaticamente
+const ul = document.getElementById('lista')
+let numeratore = 0;
+while (numeratore < listaSpesa.length) {
+    const li = document.createElement('li');
+    li.innerText = listaSpesa;
+}
