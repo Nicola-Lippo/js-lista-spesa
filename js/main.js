e5f8d0cdf17c449ebba10b70aa7b24a2d2292bf1
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-07 14:16:01 +0200

    [upd] struttura base

--- js/main.js
+++ js/main.js
@@ -5,8 +5,18 @@ const listaSpesa = ['pasta', 'pane', 'pizze', 'frutta', 'tonno', 'uova'];
 console.log(listaSpesa);
 //creo un ciclo while per leggere la lista automaticamente
 const ul = document.getElementById('lista')
+for (let i = 0; i <listaSpesa.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = listaSpesa[i];
+    li.classList.add('elemanto-lista');
+    ul.append(li);
+}
+
 let numeratore = 0;
 while (numeratore < listaSpesa.length) {
+    console.log(listaSpesa[numeratore]);
     const li = document.createElement('li');
-    li.innerText = listaSpesa;
+    li.append(listaSpesa[numeratore]);
+    ul.append(li);
+    numeratore++;
 }
